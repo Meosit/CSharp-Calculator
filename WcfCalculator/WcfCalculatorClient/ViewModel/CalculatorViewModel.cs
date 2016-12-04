58f6dfe93f704e66fb56515ed7b123236d5fa61c
Mikhail Snitavets <mksn13@gmail.com>
2016-12-05 01:04:19 +0300

    Client project created, ui added, BaseViewModel and RelayCommand added

---
+++ WcfCalculator/WcfCalculatorClient/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Windows.Input;
+
+namespace WcfCalculatorClient.ViewModel
+{
+    class CalculatorViewModel : BaseViewModel
+    {
+        private CanExecuteReference _isUiAvailable = new CanExecuteReference() {Value = true};
+    }
+
+}
---
+++ WcfCalculator/WcfCalculatorClient/ViewModel/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WcfCalculatorClient.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly CanExecuteReference _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action execute, CanExecuteReference canExecute)
+        {
+            this._execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Value;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute();
+        }
+    }
+
+
+    public class CanExecuteReference
+    {
+        public bool Value { get; set; }
+    }
+}
